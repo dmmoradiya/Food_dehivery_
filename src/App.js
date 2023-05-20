6b0018c0a5656a32af4b55185f5dc699f7cf4904
dmmoradiya <dhruv.moradiya365@gmail.com>
2023-05-20 14:54:47 +0530

    initial sourec

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Header from "./Componenets/Layout/Header";
+import Meals from "./Componenets/Meals/Meals";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Meals/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Componenets/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Fragment } from "react";
+import MealImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h2>React Delhivery</h2>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={MealImage}></img>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
