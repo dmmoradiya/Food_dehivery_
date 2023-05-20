b2a60e611b775b39beebe594fa72987fa320fb4e
dmmoradiya <dhruv.moradiya365@gmail.com>
2023-05-20 19:24:36 +0530

    Updates

---
+++ precache-manifest.9fa78db1b6c4bfae49286650c665f00c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37d82ad581f5f9e32bfc",
+    "url": "/Food_delhivery_/static/css/main.b780be85.chunk.css"
+  },
+  {
+    "revision": "37d82ad581f5f9e32bfc",
+    "url": "/Food_delhivery_/static/js/main.37d82ad5.chunk.js"
+  },
+  {
+    "revision": "419044147d971a2cec2a",
+    "url": "/Food_delhivery_/static/js/1.41904414.chunk.js"
+  },
+  {
+    "revision": "159a5398842cfcb91afa",
+    "url": "/Food_delhivery_/static/js/runtime~main.159a5398.js"
+  },
+  {
+    "revision": "55c749900b91566d7fa1eea3937e0e04",
+    "url": "/Food_delhivery_/static/media/meals.55c74990.jpg"
+  },
+  {
+    "revision": "2452c934b1db8babada35c78e2a210b7",
+    "url": "/Food_delhivery_/index.html"
+  }
+];
