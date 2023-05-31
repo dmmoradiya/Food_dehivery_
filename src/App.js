d460567401d29af50041451de25ed834fde6e0f9
dmmoradiya <dhruv.moradiya365@gmail.com>
2023-05-31 22:26:44 +0530

    ipdate cart-modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./Componenets/Layout/Header";
 import Meals from "./Componenets/Meals/Meals";
 import Cart from "./Componenets/Cart/Cart";
 
 function App() {
+
+  const [cartIsShown , setCartIsShown] = useState(false);
+
+  const showCart = () => {
+    setCartIsShown(true);
+  }
+  const hideCart =()=>{
+    setCartIsShown(false)
+  }
+
   return (
     <Fragment>
-      <Cart/>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCart}/>}
+      <Header onClick={showCart} />
       <Meals/>
     </Fragment>
   );
